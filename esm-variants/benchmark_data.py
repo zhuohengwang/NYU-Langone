7c4a4a524243953cd2c5dbaa4bc5fbe8f949602c
rubben-liu <liurubben@gmail.com>
2025-02-27 21:44:53 -0500

    process-benchmark

--- esm-variants/benchmark_data.py
+++ esm-variants/benchmark_data.py
@@ -11,6 +11,7 @@ from Bio import SeqIO
 from tqdm import tqdm
 
 import subprocess
+from pathlib import Path
 
 
 # In[2]:
@@ -25,8 +26,13 @@ preprocessed_score_path = r'./uniprot_dataset/preprocessed_uniprot_sprot_esm_sco
 # In[3]:
 
 
-benchmark_df = pd.read_csv(benchmark_path)
+benchmark_df = pd.read_csv(benchmark_path).dropna(subset=['ESM1b_score'])
+other_score_columns = benchmark_df.columns[7:25]
+benchmark_df = benchmark_df[benchmark_df[other_score_columns].notna().sum(axis=1) > 0]
 benchmark_df = benchmark_df[['uniprot_id', 'aa_change', 'ESM1b_score']]
+
+benchmark_df.to_csv(r'./uniprot_dataset/benchmark_df.csv')
+print(len(benchmark_df))
 
 uniprot_id_list = list(dict.fromkeys(benchmark_df['uniprot_id'].tolist()))
 benchmark_df.head()
@@ -53,22 +59,27 @@ print(preprocessed_dataset_length)
 # In[5]:
 
 
-subprocess.run([
-    "python", "esm_score_missense_mutations.py",
-    "--input-fasta-file", "./uniprot_dataset/preprocessed_uniprot_sprot.fasta",
-    "--output-csv-file", "./uniprot_dataset/preprocessed_uniprot_sprot_esm_scores.csv"
-], check=True)
+if not Path(preprocessed_score_path).is_file():
+    subprocess.run([
+        "python", "esm_score_missense_mutations.py",
+        "--input-fasta-file", "./uniprot_dataset/preprocessed_uniprot_sprot.fasta",
+        "--output-csv-file", "./uniprot_dataset/preprocessed_uniprot_sprot_esm_scores.csv"
+    ], check=True)
 
 
-# In[6]:
+# In[ ]:
 
 
 preprocessed_dataset_df = pd.read_csv(preprocessed_score_path)
 
-filtered_df = preprocessed_dataset_df[preprocessed_dataset_df.set_index(['seq_id', 'mut_name']).index.isin(benchmark_df.set_index(['uniprot_id', 'aa_change']).index)]
+#reproduced_df = preprocessed_dataset_df[preprocessed_dataset_df.set_index(['seq_id', 'mut_name']).index.isin(benchmark_df.set_index(['uniprot_id', 'aa_change']).index)]
+reproduced_df_filtered = preprocessed_dataset_df[
+    preprocessed_dataset_df[['seq_id', 'mut_name']].apply(tuple, axis=1).isin(
+        benchmark_df[['uniprot_id', 'aa_change']].apply(tuple, axis=1)
+    )
+]
 
-benchmark_df.to_csv(r'./uniprot_dataset/benchmark_df.csv')
-filtered_df.to_csv(r'./uniprot_dataset/filtered_df.csv')
+reproduced_df.to_csv(r'./uniprot_dataset/reproduced_df.csv')
 
 
 # In[ ]:
