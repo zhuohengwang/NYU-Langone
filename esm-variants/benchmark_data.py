93e1bd3ce4f81170fde6f8d4d0cf877c8a0d3449
rubben-liu <liurubben@gmail.com>
2025-02-28 21:41:58 -0500

    process-esm1b/esm1v-1/esm2-benchmark

--- esm-variants/benchmark_data.py
+++ esm-variants/benchmark_data.py
@@ -19,27 +19,41 @@ from pathlib import Path
 
 benchmark_path = r'./benchmarks/ClinVar_gnomAD_benchmark_with_predictions.csv'
 dataset_path = r'./uniprot_dataset/uniprot_sprot.fasta'
+benchmark_df_path = r'./uniprot_dataset/benchmark_df.csv'
 preprocessed_dataset_path = r'./uniprot_dataset/preprocessed_uniprot_sprot.fasta'
-preprocessed_score_path = r'./uniprot_dataset/preprocessed_uniprot_sprot_esm_scores.csv'
+
+# note: all models below have 33 layers and 650M params
+reproduced_dict = {'esm1b_t33_650M_UR50S' : {'name' : 'ESM1b',
+                                             'score_path' : r'./uniprot_dataset/ESM1b_reproduced_score.csv'
+                                            },
+                   'esm1v_t33_650M_UR90S_1' : {'name' : 'ESM1v-1',
+                                               'score_path' : r'./uniprot_dataset/ESM1v-1_reproduced_score.csv'
+                                              },
+                   'esm2_t33_650M_UR50D' : {'name' : 'ESM2',
+                                            'score_path' : r'./uniprot_dataset/ESM2_reproduced_score.csv'
+                                           }
+                  }
 reproduced_df_path = r'./uniprot_dataset/reproduced_df.csv'
 
 
 # In[3]:
 
 
-benchmark_df = pd.read_csv(benchmark_path).dropna(subset=['ESM1b_score'])
-other_score_columns = benchmark_df.columns[7:25]
-benchmark_df = benchmark_df[benchmark_df[other_score_columns].notna().sum(axis=1) > 0]
-benchmark_df = benchmark_df[['uniprot_id', 'aa_change', 'ESM1b_score']]
+benchmark_df = pd.read_csv(benchmark_path)
 
-benchmark_df.to_csv(r'./uniprot_dataset/benchmark_df.csv', index=False)
+# filter out row that has >1 scoring for them
+other_score_columns = benchmark_df.columns[6:25]
+benchmark_df = benchmark_df[benchmark_df[other_score_columns].notna().sum(axis=1) > 1]
+benchmark_df = benchmark_df[['uniprot_id', 'aa_change', 'ESM1b_score', 'clinvar_label']]
+
+benchmark_df.to_csv(benchmark_df_path, index=False)
 print(len(benchmark_df))
 
 uniprot_id_list = list(dict.fromkeys(benchmark_df['uniprot_id'].tolist()))
 benchmark_df.head()
 
 
-# In[ ]:
+# In[4]:
 
 
 with open(preprocessed_dataset_path, "w") as out_file:
@@ -60,36 +74,42 @@ print(preprocessed_dataset_length)
 # In[ ]:
 
 
-if not Path(preprocessed_score_path).is_file():
+for model_pretrained, model_dict in reproduced_dict.items():
     subprocess.run([
         "python", "esm_score_missense_mutations.py",
-        "--input-fasta-file", "./uniprot_dataset/preprocessed_uniprot_sprot.fasta",
-        "--output-csv-file", "./uniprot_dataset/preprocessed_uniprot_sprot_esm_scores.csv"
+        "--input-fasta-file", preprocessed_dataset_path,
+        "--output-csv-file", model_dict['score_path'],
+        "--model-name", model_pretrained,
     ], check=True)
 
 
-# In[11]:
+# In[5]:
 
 
-if not Path(reproduced_df_path).is_file():
-    preprocessed_dataset_df = pd.read_csv(preprocessed_score_path)
+model_df_list = []
+for model_pretrained, model_dict in reproduced_dict.items():
+    score_df = pd.read_csv(model_dict['score_path'])
+
+    score_df = score_df.rename(columns={'esm_score' : model_dict['name'] + '_score'})
     
-    reproduced_df = preprocessed_dataset_df[
-        preprocessed_dataset_df[['seq_id', 'mut_name']].apply(tuple, axis=1).isin(
+    reproduced_df = score_df[
+        score_df[['seq_id', 'mut_name']].apply(tuple, axis=1).isin(
             benchmark_df[['uniprot_id', 'aa_change']].apply(tuple, axis=1)
         )
     ]
-else:
-    reproduced_df = pd.read_csv(reproduced_df_path)
 
-reproduced_df = reproduced_df.set_index(['seq_id', 'mut_name']).reindex(
-    benchmark_df.set_index(['uniprot_id', 'aa_change']).index, 
-    fill_value=None
-).reset_index().rename(columns={'uniprot_id': 'seq_id', 'aa_change': 'mut_name'})[['seq_id', 'mut_name', 'esm_score']].dropna(subset=['esm_score'])
+    reproduced_df = reproduced_df.set_index(['seq_id', 'mut_name']).reindex(
+        benchmark_df.set_index(['uniprot_id', 'aa_change']).index, 
+        fill_value=None
+    ).reset_index().rename(
+        columns={'uniprot_id': 'seq_id', 'aa_change': 'mut_name'}
+    )[['seq_id', 'mut_name', model_dict['name'] + '_score']].dropna(subset=[model_dict['name'] + '_score'])
 
+    print(len(reproduced_df))
+    model_df_list.append(reproduced_df)
 
-print(len(reproduced_df))
-reproduced_df.to_csv(reproduced_df_path, index=False)
+merge_df = reduce(lambda left, right: pd.merge(left, right, on=['seq_id', 'mut_name'], how='inner'), model_df_list)
+merge_df.to_csv(reproduced_df_path, index=False)
 
 
 # In[ ]:
